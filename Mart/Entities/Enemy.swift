2f673583dea3fa985592eedf998f0b345a18e9e7
Suyeol Jeon <devxoul@gmail.com>
2015-11-15 05:11:51 +0900

    Add Enemy.swift

---
+++ Mart/Entities/Enemy.swift
@@ -0,0 +1,26 @@
+//
+//  Enemy.swift
+//  Mart
+//
+//  Created by 전수열 on 11/15/15.
+//  Copyright © 2015 Suyeol Jeon. All rights reserved.
+//
+
+import SpriteKit
+
+class Enemy: SKSpriteNode {
+
+    var velocity: CGFloat = 1
+
+    convenience init() {
+        self.init(imageNamed: "Spaceship")
+        self.size = CGSize(width: 40, height: 40)
+        self.velocity = randomf(1, 3)
+
+        if let filter = CIFilter(name: "CIHueAdjust") {
+            filter.setValue(2, forKey: "inputAngle")
+            self.texture = self.texture?.textureByApplyingCIFilter(filter)
+        }
+    }
+    
+}
